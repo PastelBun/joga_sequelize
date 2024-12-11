c8ccb324c693e50e3609574737e80726b6120989
Sam <djcsdj@hotmail.com>
2024-12-11 08:48:40 +0200

    Got all of the article data and displayed it

--- controllers/article.js
+++ controllers/article.js
@@ -2,11 +2,18 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize') // Example for sqlite
 
 const { Article }= require('../models');
+const { error } = require('console');
 
 const getAllArticles=(req, res)=>{
-    const articles=Article.findAll()
-    res.json(articles)
-}
+    Article.findAll()
+    .then(articles =>{
+        console.log(articles)
+        return res.status(200).json({ articles });
+    })
+    .catch (error => {
+        return res.status(500).send(error.message);
+    })
+};
 
 module.exports={
     getAllArticles
