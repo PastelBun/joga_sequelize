958a6b8f1a18e4be0e7b0180e52c25605501bda3
Sam <djcsdj@hotmail.com>
2024-12-11 11:48:09 +0200

    edit working, delete not

--- controllers/admin/article.js
+++ controllers/admin/article.js
@@ -23,7 +23,49 @@ const newArticle=models.Article.create({
     return res.status(500).send(error.message);
 })
 }
-
+const updateArticle=( req, res )=>{
+    const id=parseInt(req.params.id)
+    let name=req.body.name
+    let slug=req.body.slug
+    let image=req.body.image
+    let body=req.body.body
+    
+    const newArticle=models.Article.update({
+        name:name,
+        slug:slug,
+        image:image,
+        body:body,
+        published: new Date().toISOString().slice(0, 19).replace('T', ' ')
+    },
+    {
+        where: { id:id },
+    })
+    .then(article=>{
+        console.log(article)
+        return res.status(200).json({ message: "Article updated"});
+    })
+    .catch (error=>{
+        return res.status(500).send(error.message);
+    })
+}
+const deleteArticle=( req, res )=>{
+        
+        const id= parseInt(req.params.id)
+        const deletedArticle=models.Article.delete(
+            {
+                where: {id:id}
+            }
+        )
+        .then(deletedCount=>{
+            if (deletedCount>0){
+            return res.status(200).json({ message: "Article deleted"});
+        }})
+        .catch (error=>{
+            return res.status(500).send(error.message);
+        })
+}
 module.exports={
-    createArticle
+    createArticle,
+    updateArticle,
+    deleteArticle
 };
