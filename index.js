f96a73f44a26c99ca7a5259c85007428050576d0
Sam <djcsdj@hotmail.com>
2024-12-11 08:58:46 +0200

    get article by slug working

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ sequelize
 
 const articleRouter=require('./routes/article');
 app.use('/', articleRouter);
+app.use('/article', articleRouter)
 
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
